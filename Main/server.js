f262162191704f0c3e414fa472127fa01da52ca4
Brett Winterstrom <brettwinterstrom@yahoo.com>
2020-12-17 19:06:47 -0800

    test

--- Main/server.js
+++ Main/server.js
@@ -13,26 +13,26 @@ app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-const server = http.createServer(handleRequest);
+// const server = http.createServer(handleRequest);
 
-function handleRequest(req, res) {
-    var path = req.url;
+// function handleRequest(req, res) {
+//     var path = req.url;
 
-    switch (path) {
-        case "/notes":
-            return fs.readFile(__dirname + "/public/notes.html", function (err, data) {
-                if (err) throw err;
-                res.writeHead(200, { "Content-Type": "text/html" });
-                res.end(data);
-            });
-        default:
-            return fs.readFile(__dirname + "/public/index.html", function (err, data) {
-                if (err) throw err;
-                res.writeHead(200, { "Content-Type": "text/html" });
-                res.end(data);
-            });
-    }
-}
+//     switch (path) {
+//         case "/notes":
+//             return fs.readFile(__dirname + "/public/notes.html", function (err, data) {
+//                 if (err) throw err;
+//                 res.writeHead(200, { "Content-Type": "text/html" });
+//                 res.end(data);
+//             });
+//         default:
+//             return fs.readFile(__dirname + "/public/index.html", function (err, data) {
+//                 if (err) throw err;
+//                 res.writeHead(200, { "Content-Type": "text/html" });
+//                 res.end(data);
+//             });
+//     }
+// }
 
 
 app.listen(PORT, function () {
